5dc6d2179837714bb8392a232276be22174206fe
Jarrod Seccombe <jarrod.seccombe@cps.com>
2023-03-07 11:03:43 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint/eslint-plugin',
+        'import',
+        'prettier'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/typescript',
+        'plugin:prettier/recommended',
+        'xo',
+        'xo-typescript'
+    ],
+    rules: {
+        "comma-dangle": "off",
+        "object-curly-spacing": "off",
+        "operator-linebreak": "off",
+        "no-mixed-spaces-and-tabs": "off",
+        "@typescript-eslint/comma-dangle": "off",
+        "@typescript-eslint/indent": "off",
+        "@typescript-eslint/quotes": "off",
+        "@typescript-eslint/semi": "off",
+        "@typescript-eslint/object-curly-spacing": "off",
+        "no-unused-vars": "off",
+        "no-warning-comments": "off",
+        "no-promise-executor-return": "off"
+    }
+}
